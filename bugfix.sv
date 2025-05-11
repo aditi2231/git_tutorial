a7fee989b55c20d695a0de03fbe9c86a7316776f
aditi2231 <aditi16adil@gmail.com>
2025-05-11 17:07:14 +0530

    bug fix

---
+++ bugfix.sv
@@ -0,0 +1,16 @@
+module d_flip_flop (
+    input wire d,
+    input wire clk,
+    input wire reset,
+    output reg q
+);
+
+always @(posedge clk or posedge reset) begin
+    if (reset) begin
+        q <= 1'b0;
+    end else begin
+        q <= d;
+    end
+end
+
+endmodule
