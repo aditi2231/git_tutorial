3718b21491c6b96a99f178417402240f8413b639
aditi2231 <aditi16adil@gmail.com>
2025-05-11 16:40:33 +0530

    feature commit 2

--- feauture.sv
+++ feauture.sv
@@ -6,3 +6,14 @@ module mux_2x1 (
 );
     assign y = (sel) ? b : a;
 endmodule
+
+
+module decoder_4x2 (
+    input wire [1:0] in,
+    output wire [3:0] out
+);
+    assign out[0] = ~in[1] & ~in[0];
+    assign out[1] = ~in[1] & in[0];
+    assign out[2] = in[1] & ~in[0];
+    assign out[3] = in[1] & in[0];
+endmodule
